f89d3e347897281dc46a0033ec1146fda8e88a0d
John Gerrard Holland <john_holland1@brown.edu>
2023-08-24 15:07:06 +0200

    chore: remove duplicated parts of Layout

--- src/@thepolicylab-projectportals/gatsby-theme-project-portal/layouts/Layout.tsx
+++ src/@thepolicylab-projectportals/gatsby-theme-project-portal/layouts/Layout.tsx
@@ -1,102 +1,23 @@
-import React, { FunctionComponent, ReactNode } from "react"
-import { ImageDataLike } from "gatsby-plugin-image"
+import React, { FunctionComponent } from "react"
 
 import {
-  Footer,
-  BottomBanner,
-  DevelopmentBanner,
-  Navbar,
-} from "@thepolicylab-projectportals/gatsby-theme-project-portal/src/components"
+  Layout as ThemeLayout,
+  LayoutProps as ThemeLayoutProps,
+} from "@thepolicylab-projectportals/gatsby-theme-project-portal/src/layouts/Layout"
 
 import { GovBanner } from "../../../components/GovBanner"
 
-export interface ThemeLayoutProps {
-  path: string
-  title: string
-  description: string
-  children: ReactNode
-  data: {
-    site: {
-      siteMetadata: {
-        title: string
-      }
-    }
-    projectPortalConfig: {
-      showDevBanner?: boolean
-      pages: {
-        name: string
-        link: string
-        show: boolean
-      }[]
-      staticText: {
-        bottomBanner: {
-          text: string
-          link: string
-        }
-        footer: {
-          copyright: string
-          links: {
-            title: string
-            link: string
-          }[]
-          heading: {
-            title: string
-            link: string
-          }
-        }
-      }
-    }
-    navbarLogo?: ImageDataLike
-    bottomBannerLogo?: ImageDataLike
-    footerLogo?: ImageDataLike
-  }
-}
-
 export const Layout: FunctionComponent<ThemeLayoutProps> = ({
   path,
-  data: {
-    site: {
-      siteMetadata: { title: siteTitle },
-    },
-    projectPortalConfig: {
-      showDevBanner,
-      pages,
-      staticText: { bottomBanner, footer },
-    },
-    navbarLogo,
-    bottomBannerLogo,
-    footerLogo,
-  },
+  data,
   children,
 }) => {
   return (
     <>
       <GovBanner />
-      <div className="w-full mx-0 bg-white border-0 xl:container xl:p-0 xl:mx-auto xl:border-l xl:border-r xl:border-gray-200 flex flex-col min-h-screen">
-        {showDevBanner && <DevelopmentBanner />}
-        <Navbar
-          title={siteTitle}
-          image={navbarLogo}
-          pages={pages}
-          activePage={path}
-        />
-        <div className="flex-1">{children}</div>
-        <BottomBanner
-          text={bottomBanner.text}
-          link={bottomBanner.link}
-          linkId={"bottomBannerLink"}
-          image={bottomBannerLogo}
-        />
-        <Footer
-          heading={footer.heading}
-          copyright={footer.copyright}
-          links={footer.links}
-          image={{
-            imageData: footerLogo,
-            altText: siteTitle + " logo",
-          }}
-        />
-      </div>
+      <ThemeLayout path={path} data={data}>
+        {children}
+      </ThemeLayout>
     </>
   )
 }

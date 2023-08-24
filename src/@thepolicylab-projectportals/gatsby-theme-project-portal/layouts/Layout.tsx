2403f67c0507e70e41df72201258b55641050069
John Gerrard Holland <john_holland1@brown.edu>
2023-08-24 14:42:58 +0200

    fix: update Layout to fully re-implement the theme

--- src/@thepolicylab-projectportals/gatsby-theme-project-portal/layouts/Layout.tsx
+++ src/@thepolicylab-projectportals/gatsby-theme-project-portal/layouts/Layout.tsx
@@ -1,49 +1,165 @@
 import React, { FunctionComponent, ReactNode } from "react"
+import { graphql } from "gatsby"
+import { ImageDataLike } from "gatsby-plugin-image"
+
 import {
   Footer,
   BottomBanner,
+  DevelopmentBanner,
   Navbar,
-  SiteMetadata,
-  DevelopmentBanner
 } from "@thepolicylab-projectportals/gatsby-theme-project-portal/src/components"
 
 import { GovBanner } from "../../../components/GovBanner"
 
-import { useProjectPortalConfig } from "@thepolicylab-projectportals/gatsby-theme-project-portal/src/hooks"
-
-interface LayoutProps {
-  activePage?: string
+export interface ThemeLayoutProps {
+  path: string
   title: string
   description: string
   children: ReactNode
-  showDevBanner?: boolean
+  data: {
+    site: {
+      siteMetadata: {
+        title: string
+      }
+    }
+    projectPortalConfig: {
+      showDevBanner?: boolean
+      pages: {
+        name: string
+        link: string
+        show: boolean
+      }[]
+      staticText: {
+        bottomBanner: {
+          text: string
+          link: string
+        }
+        footer: {
+          copyright: string
+          links: {
+            title: string
+            link: string
+          }[]
+          heading: {
+            title: string
+            link: string
+          }
+        }
+      }
+    }
+    navbarLogo?: ImageDataLike
+    bottomBannerLogo?: ImageDataLike
+    footerLogo?: ImageDataLike
+  }
 }
 
-function getShowDevBannerSetting(showDevBanner?: boolean) {
-  // Checks whether the "showDevBanner" argument is explicitly set and use it if it is (i.e. not null),
-  // else use the showDevBannerThemeSetting.
-  const { showDevBanner: showDevBannerThemeSetting } = useProjectPortalConfig()
-  return showDevBanner ?? showDevBannerThemeSetting
-}
-
-export const Layout: FunctionComponent<LayoutProps> = ({
-  activePage,
-  title,
-  description,
-  showDevBanner,
+export const Layout: FunctionComponent<ThemeLayoutProps> = ({
+  path,
+  data: {
+    site: {
+      siteMetadata: { title: siteTitle },
+    },
+    projectPortalConfig: {
+      showDevBanner,
+      pages,
+      staticText: { bottomBanner, footer },
+    },
+    navbarLogo,
+    bottomBannerLogo,
+    footerLogo,
+  },
   children,
 }) => {
-  const coalescedShowDevBanner = getShowDevBannerSetting(showDevBanner)
-
   return (
-    <div className="w-full mx-0 bg-white border-0 xl:container xl:p-0 xl:mx-auto xl:border-l xl:border-r xl:border-gray-200 flex flex-col min-h-screen">
-      {coalescedShowDevBanner && <DevelopmentBanner />}
-      <GovBanner/>
-      <SiteMetadata title={title} description={description} />
-      <Navbar activePage={activePage} />
-      <div className="flex-1">{children}</div>
-      <BottomBanner />
-      <Footer />
-    </div>
+    <>
+      <GovBanner />
+      <div className="w-full mx-0 bg-white border-0 xl:container xl:p-0 xl:mx-auto xl:border-l xl:border-r xl:border-gray-200 flex flex-col min-h-screen">
+        {showDevBanner && <DevelopmentBanner />}
+        <Navbar
+          title={siteTitle}
+          image={navbarLogo}
+          pages={pages}
+          activePage={path}
+        />
+        <div className="flex-1">{children}</div>
+        <BottomBanner
+          text={bottomBanner.text}
+          link={bottomBanner.link}
+          linkId={"bottomBannerLink"}
+          image={bottomBannerLogo}
+        />
+        <Footer
+          heading={footer.heading}
+          copyright={footer.copyright}
+          links={footer.links}
+          image={{
+            imageData: footerLogo,
+            altText: siteTitle + " logo",
+          }}
+        />
+      </div>
+    </>
   )
 }
+
+export const query = graphql`
+  fragment LayoutData on Query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    projectPortalConfig {
+      showDevBanner
+      pages {
+        link
+        name
+        show
+      }
+      staticText {
+        bottomBanner: bottom_banner {
+          text
+          link
+        }
+        footer {
+          copyright
+          links {
+            title
+            link
+          }
+          heading {
+            title
+            link
+          }
+        }
+      }
+    }
+    navbarLogo: file(
+      name: { eq: "navbar" }
+      extension: { in: ["png", "jpg", "jpeg"] }
+      sourceInstanceName: { eq: "themeImages" }
+    ) {
+      childImageSharp {
+        gatsbyImageData(height: 64)
+      }
+    }
+    bottomBannerLogo: file(
+      name: { eq: "bottom-banner" }
+      extension: { in: ["png", "jpg", "jpeg"] }
+      sourceInstanceName: { eq: "themeImages" }
+    ) {
+      childImageSharp {
+        gatsbyImageData(width: 160)
+      }
+    }
+    footerLogo: file(
+      name: { eq: "footer" }
+      extension: { in: ["png", "jpg", "jpeg"] }
+      sourceInstanceName: { eq: "themeImages" }
+    ) {
+      childImageSharp {
+        gatsbyImageData(height: 64)
+      }
+    }
+  }
+`
